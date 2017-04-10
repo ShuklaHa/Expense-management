1e3086b5891fc62c76b998a3cfa39262d152868d
ShuklaHa <harshit.shukla31@yahoo.com>
2017-04-10 13:14:56 +0530

    Removed comments

--- ExpensesManagement/src/main/java/com/room/expenses/ExpensesManagement/App.java
+++ ExpensesManagement/src/main/java/com/room/expenses/ExpensesManagement/App.java
@@ -3,10 +3,7 @@ package com.room.expenses.ExpensesManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
+
 @SpringBootApplication
 public class App 
 {
